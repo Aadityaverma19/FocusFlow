21bcf76ed4cacc3cb519322670d7ee8d9c5bc187
Aaditya <vermaservices19@gmail.com>
2026-05-06 23:06:13 +0530

    adding created date and time paramter to tasks

--- src/main/java/com/focusflow/focusflow_backend/dto/TaskResponse.java
+++ src/main/java/com/focusflow/focusflow_backend/dto/TaskResponse.java
@@ -1,14 +1,53 @@
 package com.focusflow.focusflow_backend.dto;
 
+import java.time.LocalDateTime;
+
+import org.springframework.cglib.core.Local;
+
 public class TaskResponse {
-    public Long id;
-    public String title;
-    public String description;
-    public Boolean completed;
-    public TaskResponse(Long id, String title, String description, Boolean completed) {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private LocalDateTime createdAt;
+    
+    public TaskResponse(Long id, String title, String description, Boolean completed, LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.completed = completed;
-    }    
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Boolean getCompleted() {
+        return completed;
+    }
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    
 }   
